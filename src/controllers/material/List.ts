fde618b14fd92627661e93a3caad6cd1439478d7
Elias O_o <grumec951@gmail.com>
2022-02-03 22:46:18 -0300

    Create controller List materials

---
+++ src/controllers/material/List.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+import ListMaterialService from '../../services/material/List'
+
+export default class ListMaterialControl {
+  async handle(request: Request, response: Response) {
+    const listMaterialService = new ListMaterialService()
+
+    const material = await listMaterialService.execute().finally()
+
+    return response.send(material)
+  }
+}

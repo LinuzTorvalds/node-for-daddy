3315b2a6b13a33d660f32ce99aaa7629947d22fe
Elias O_o <grumec951@gmail.com>
2022-02-03 22:12:26 -0300

    Modify ports

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app'
 
-app.listen(process.env.PORT || 3001, () =>
+app.listen(process.env.PORT || 3005, () =>
   console.log(`Pai ta on familia o_O on PORT ${process.env.PORT}`)
 )

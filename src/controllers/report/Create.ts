6259574d4c98c56a6995f1a4d179a6950be436da
Elias O_o <grumec951@gmail.com>
2022-02-05 20:12:45 -0300

    Create controller from create and update reports

---
+++ src/controllers/report/Create.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import CreateReportService from '../../services/report/Create'
+
+export default class CreateReportControl {
+  async handle(request: Request, response: Response) {
+    const { Lote, Description, Amount, Day } = request.body
+
+    const createReportService = new CreateReportService()
+
+    const report = await createReportService
+      .execute({ Lote, Description, Amount, Day })
+      .finally()
+
+    return response.send(report)
+  }
+}
---
+++ src/controllers/report/Update.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import UpdateReportService from '../../services/report/Update'
+
+export default class UpdateReportControl {
+  async handle(request: Request, response: Response) {
+    const { Code } = request.params
+    const { Lote, Description, Amount, Day } = request.body
+
+    const updateReportService = new UpdateReportService()
+
+    await updateReportService
+      .execute(Code, { Lote, Description, Amount, Day })
+      .finally()
+
+    return response.status(201).send('successfully updated :D')
+  }
+}

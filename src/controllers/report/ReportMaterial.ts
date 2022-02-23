d5f750758b5ba669baaa927a008db8b74fa70bcd
John Wick <grumec92@gmail.com>
2022-02-23 18:55:40 -0300

    Bicho burro 0-0

--- src/controllers/report/ReportMaterial.ts
+++ src/controllers/report/ReportMaterial.ts
@@ -3,14 +3,14 @@ import ReportMaterialService from '../../services/report/ReportMaterial'
 
 export default class ReportMaterialControl {
   async handle(request: Request, response: Response) {
-    const { Description, Lote, StartDay, EndDay } = request.body
+    const { Description, Lote, Days, EndDay } = request.body
 
     const reportMaterialService = new ReportMaterialService()
 
     const report = await reportMaterialService.execute({
       Description,
       Lote,
-      StartDay,
+      Days,
       EndDay,
     })
 
